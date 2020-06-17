5217058b6365ecf31dbfbe61da62f20b53cfd582
Bryce Whittington <brycew30@gmail.com>
2020-06-17 17:17:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,4 @@
 def badge_maker(name)
   puts "Hello, my name is #{name}"
 end
-  
-assign_rooms = 1
-  puts "Hello, #{name}! You'll be assigned to room #{assign_rooms}!"
-  assign_rooms += 1
-end
+
