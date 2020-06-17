6846ebcd28fda9edc6edfe1132cb34a05317ba67
Bryce Whittington <brycew30@gmail.com>
2020-06-17 17:38:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,9 @@
- def badge_maker(name)
+def badge_maker(name)
    return "Hello, my name is #{name}."
 end
 @speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
  
- def batch_badge_creator(speakers)
+def batch_badge_creator(speakers)
    @speakers.collect {|name| badge_maker(name)}
 end
 
