eaba976ecb89a093633a9148f7e4a0207d72954c
Bryce Whittington <brycew30@gmail.com>
2020-06-17 17:36:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,13 @@
  def badge_maker(name)
    return "Hello, my name is #{name}."
 end
- speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+@speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
  
  def batch_badge_creator(speakers)
-   speakers.collect {|name| badge_maker(name)}
+   @speakers.collect {|name| badge_maker(name)}
 end
 
 def assign_rooms(speakers)
-  speakers.each_with_index {|speaker|, index}
+  @speakers.each_with_index {|speaker|, index}
 end
   
