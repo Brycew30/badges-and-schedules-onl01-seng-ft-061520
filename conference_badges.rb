0072c3d655ddd688beff55dd3eaa41db91394e74
Bryce Whittington <brycew30@gmail.com>
2020-06-17 17:14:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  speakers.each do |name|
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+  
+assign_rooms = 1
+  puts "Hello, #{name}! You'll be assigned to room #{assign_rooms}!"
+  assign_rooms += 1
+end
